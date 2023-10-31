9c3186c88b84236c5b628a843e38ada5f1db4580
itspeetah <pietro.g.moroni@gmail.com>
2023-10-31 18:21:21 +0100

    Update mockmap.js

--- mock-map-buttons/js/mockmap.js
+++ mock-map-buttons/js/mockmap.js
@@ -2,34 +2,11 @@
 const imageRatio = 3 / 2;
 
 // Data
-// const points = [
-//   { x: 10, y: 10, data: "Point 0" },
-//   { x: 60, y: 10, data: "Point 1" },
-//   { x: 50, y: 30, data: "Point 2" },
-//   { x: 70, y: 90, data: "Point 3" },
-// ];
-
 const points = [
-  {
-    x: 10,
-    y: 10,
-    data: "https://sato-code.com/en/escape-games/escape-room-bellinzona",
-  },
-  {
-    x: 60,
-    y: 10,
-    data: "https://sato-code.com/en/escape-games/escape-room-lugano",
-  },
-  {
-    x: 50,
-    y: 30,
-    data: "https://sato-code.com/en/escape-games/escape-room-pavia",
-  },
-  {
-    x: 70,
-    y: 90,
-    data: "https://sato-code.com/en/escape-games/escape-room-lecco",
-  },
+  { x: 10, y: 10, data: "Point 0" },
+  { x: 60, y: 10, data: "Point 1" },
+  { x: 50, y: 30, data: "Point 2" },
+  { x: 70, y: 90, data: "Point 3" },
 ];
 
 // Elements
@@ -46,10 +23,10 @@ function createMapButtons() {
   points.forEach((point) => {
     const el = document.createElement("a");
     el.className = "map__button";
-    // el.onclick = (_e) => {
-    //   alert(point.data);
-    // };
-    el.href = point.data;
+    el.onclick = (_e) => {
+      alert(point.data);
+    };
+    // el.href = point.data;
     el.style.left = `${point.x}%`;
     el.style.top = `${point.y}%`;
     map.appendChild(el);
